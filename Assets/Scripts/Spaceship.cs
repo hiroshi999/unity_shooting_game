d121f5d722e550bcc0bd187a3fb5db0a9658bb2b
takada <qwer4404@gmail.com>
2014-12-22 01:19:55 +0900

    first commit

---
+++ Assets/Scripts/Spaceship.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Spaceship : MonoBehaviour {
+
+	public float speed;
+
+	public float shotDelay;
+
+	public GameObject bullet;
+
+	public bool canShot;
+
+	public GameObject explosion;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Explosion(){
+		Instantiate (explosion, transform.position, transform.rotation);
+	}
+
+	public void Shot (Transform origin) {
+		Instantiate (bullet, origin.position, origin.rotation);
+	}
+
+	public Animator GetAnimator()
+	{
+		return animator;
+	}
+}
